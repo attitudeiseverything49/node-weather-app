ac544a7d7b126dbefeb886be51d5c1dbc267d48f
Shahul Riyaz <attitudeiseverything49@gmail.com>
2021-07-06 22:22:59 +0530

    added lat and long

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const weatherreport = (access_key, latitude, lon, u, callback) =>
         else if(body.error)
         callback(body.error.info,undefined)
         else
-        callback(undefined,`The weather forecast is ${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degress outside. It feels like ${body.current.feelslike} degree out. The wind speed is ${body.current.wind_speed}km/h.`)
+        callback(undefined,`The weather forecast is ${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degress outside. It feels like ${body.current.feelslike} degree out. The wind speed is ${body.current.wind_speed}km/h. Latitude is ${latitude} and longitude is ${lon}`)
     })
 }
 module.exports.weatherreport = weatherreport
